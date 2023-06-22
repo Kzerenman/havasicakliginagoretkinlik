295403dede2823f5fd3b7960e0bdf2cc2e97902d
Kzerenman <131655266+Kzerenman@users.noreply.github.com>
2023-06-22 22:33:21 +0300

    Add files via upload

---
+++ havasicakliginegoretkinlik/src/havasicakligi.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class havasicakligi {
+    public static void main(String[] args) {
+        int heat;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sıcaklık Giriniz : ");
+        heat = input.nextInt();
+
+        if(heat < 5){
+            System.out.println("Kayak Yapabilirsiniz.");
+        }else if (heat >= 5 && heat <= 25){
+            if (heat <= 15){
+                System.out.println("Sinemaya Gidebilirsiniz. ");
+            }
+            if (heat >= 10){
+                System.out.println("Pikniğe gidebilirsiniz.");
+            }
+            else{
+                System.out.println("Yüzmeye Gidebilirsiniz.");
+
+            }
+        }
+
+        }
+    }
+
